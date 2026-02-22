8db8a6e31421fc1fea1681c0754e0cb56d0214cd
Filippo Santovito <>
2026-02-22 18:33:06 +0100

    fix numeric string values parsing

--- src/asn1decoder/asn1values/numeric_string.py
+++ src/asn1decoder/asn1values/numeric_string.py
@@ -1,21 +1,29 @@
 import string
+from typing import List
 from asn1decoder.asn1types import ASN1Encoding, EncodingType
 from asn1decoder.asn1parser import ASN1ParserError
-from asn1decoder.asn1values.octet_string import parse_octet_string
+from asn1decoder.asn1values.octet_string import parse_octetstring
 
 
 class NumericStringParserError(ASN1ParserError):
     pass
 
 
-def is_valid_char(char: str) -> bool:
-    return char in (string.digits + " ")
+VALID_CHARS = string.digits + " "
 
 
-def first_invalid_char(chars: str) -> str | None:
-    for char in chars:
-        if not is_valid_char(char):
-            return char
+def decode_byte(value: int) -> str:
+    if not 0 <= value <= 255:
+        raise ValueError(f"invalid byte '{value.to_bytes().hex()}'")
+    try:
+        char = value.to_bytes().decode("ascii")
+    except UnicodeDecodeError:
+        raise ValueError(
+            f"byte '{value.to_bytes().hex()}' is not valid for a NumericString"
+        )
+    if char not in VALID_CHARS:
+        raise ValueError(f"char '{char}' is not valid for a NumericString")
+    return char
 
 
 def parse_primitive_numericstring(encoding: ASN1Encoding) -> str:
@@ -28,29 +36,28 @@ def parse_primitive_numericstring(encoding: ASN1Encoding) -> str:
     if encoding.content is None:
         return ""
 
-    chars = encoding.content.decode("ascii")
+    try:
+        chars = [decode_byte(byte) for byte in encoding.content]
+    except ValueError as e:
+        raise NumericStringParserError(str(e))
 
-    invalid_char = first_invalid_char(chars)
-    if invalid_char is not None:
-        raise NumericStringParserError(
-            f"NumericString with invalid char '{invalid_char}'."
-        )
-
-    return chars
+    return "".join(chars)
 
 
 def parse_constructed_numericstring(encoding: ASN1Encoding) -> str:
     if encoding.inner_encodings is None:
         raise NumericStringParserError("NumericString with invalid octets string")
 
-    chars = []
+    chars: List[str] = []
+
     for inner_encoding in encoding.inner_encodings:
-        char = parse_octet_string(inner_encoding)
+        data = parse_octetstring(inner_encoding)
 
-        if not is_valid_char(char):
-            raise NumericStringParserError(f"NumericString with invalid char ({char})")
+        try:
+            chars.extend([decode_byte(byte) for byte in data])
+        except ValueError as e:
+            raise NumericStringParserError(str(e))
 
-        chars.append(char)
     return "".join(chars)
 
 
